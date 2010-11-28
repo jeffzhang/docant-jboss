4caca8794ee2a1c0f9b093dc0a4746b54c28cd70
jeffzhang <jeff.zhang@jboss.org>
2010-11-28 14:31:13 +0800

    init commit, add ironjacamar document

---
+++ env.sh
@@ -0,0 +1,19 @@
+
+export LOCAL_GIT_HOST=beijing
+export LOCAL_GIT_USER=shihang
+export LOCAL_GIT_URL=ssh://$LOCAL_GIT_USER@$LOCAL_GIT_HOST/my/
+export GITHUB_URL=
+
+# jboss as 6 community document
+export JBOSSAS_SVN=http://anonsvn.jboss.org/repos/jbossas/projects/docs/community/6/
+
+# ironjacamar (jboss jca)
+# userguide developerguide
+export IRONJACAMAR_SVN=http://anonsvn.jboss.org/repos/jbossas/projects/jboss-jca/trunk/doc
+export IRONJACAMAR_GIT=svn/jboss/jboss-jca
+
+# hornetq
+export HORNETQ_SVN=http://anonsvn.jboss.org/repos/hornetq/trunk/docs/
+export HORNETQ_GIT=svn/jboss/hornetq
+
+
